c1fd19428a2fd255ca2a50a291a10ffcde8093ec
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-22 17:53:51 +0100

    Basic input and output app

---
+++ MyfirstApp/BasicInOutApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicInOutApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Welcome user to app
+
+        Console.WriteLine("Welcome to my first application");
+
+        // Ask user for first name
+
+        Console.Write("What is your firstname? ");
+        string firstName = Console.ReadLine();
+
+        // Greet user by firstname
+
+        Console.WriteLine($"Hello {firstName} , thanks for visiting my app!!");
+        Console.ReadLine();
+    }
+}
+
