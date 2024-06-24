3f8f8b049bbf6272a3796149c625b29b7108e927
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-25 00:31:07 +0100

    Basic type conversion app

---
+++ TypeConversionApp/TypeConversion/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TypeConversion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your age: ");
+        string? ageText = Console.ReadLine();
+
+        //int age = int.Parse(ageText);
+
+        bool isValidInt = int.TryParse(ageText, out int age);
+        Console.WriteLine($"The value entered is valid int convertible? : {isValidInt} --- and it's value is {ageText} ");
+        Console.ReadLine();
+    }
+}
+
