5e869f6a30a5033592638d7c6d14a6c0910de92e
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-24 22:01:42 +0100

    Basic null datatype app

---
+++ NullVariableApp/NullVariable/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NullVariable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int? age = null;
+        decimal? averageBatting = null;
+        double? accountBalance = null;
+        string? firstName = null;
+
+        Console.WriteLine(firstName);
+        Console.ReadLine();
+    }
+}
+
