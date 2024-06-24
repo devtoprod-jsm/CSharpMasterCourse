e2f3c48dbdfcb6b869cbca7074121df1680dc411
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-24 21:24:27 +0100

    Basic double datatype app

---
+++ DoubleVariableApp/DoubleVariable/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DoubleVariables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double averageAge;
+
+        averageAge = (37.0 + 6 + 31) / 3;
+        Console.WriteLine(averageAge);
+        Console.ReadLine();
+    }
+}
