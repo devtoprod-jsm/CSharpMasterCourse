3c29a84513a59c517363806bd9850d99373a0bd4
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-24 11:14:55 +0100

    Solution for string datatype application.

---
+++ StringDataTypeApp/StringDataType/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StringDataType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = string.Empty;
+        string lastName = string.Empty;
+
+        Console.Write("Please enter your firstname: ");
+        firstName = Console.ReadLine();
+        Console.Write("Pleae enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine(@$"The home folder for the user is C:\Users\{firstName}");
+        Console.ReadLine();
+    }
+}
+
