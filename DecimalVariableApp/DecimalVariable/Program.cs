77fb2d5f3c24d93cbc1d67ad0d16cf8198f490c5
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-24 21:34:43 +0100

    Basic decimal datatype app

---
+++ DecimalVariableApp/DecimalVariable/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DecimalVariable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal balanceInAccount;
+        balanceInAccount = 2.34M;
+        Console.WriteLine(balanceInAccount);
+        Console.ReadLine();
+    }
+}
+
