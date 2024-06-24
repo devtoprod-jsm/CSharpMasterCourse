142977d879dd68d43ac9944b6e1f4f147e543b8c
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-24 20:20:01 +0100

    Basic int app

---
+++ IntVariablesApp/IntVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace IntVariables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 37;
+        int ageAfterTenYears;
+        ageAfterTenYears = age + 10;
+        Console.WriteLine(ageAfterTenYears / 2);
+        Console.ReadLine();
+    }
+}
+
