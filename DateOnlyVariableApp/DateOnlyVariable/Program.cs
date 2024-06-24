d5ce20d168dcb50ab485e50c127b192e46f79881
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-25 00:07:15 +0100

    Basic DateOnly datatype app

---
+++ DateOnlyVariableApp/DateOnlyVariable/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DateOnlyVariable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime today = DateTime.Now;
+        DateOnly birthday = DateOnly.Parse("09/20/2018");
+
+        Console.WriteLine($"date in datetime : {today.Date}");
+        Console.WriteLine($"date in dateonly : {birthday}");
+        Console.WriteLine($"dateonly formatting : {birthday.ToString("MMMM dd, yyyy")}");
+        Console.ReadLine();
+    }
+}
+
