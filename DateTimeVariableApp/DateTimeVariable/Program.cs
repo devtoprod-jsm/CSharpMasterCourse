4f9f7217dfb0e5b3b60655a97f9c95fb47ab0087
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-24 23:54:21 +0100

    Basic DateTime datatype app

---
+++ DateTimeVariableApp/DateTimeVariable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace DateTimeVariable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime today = DateTime.Now;
+        Console.WriteLine($"today: {today}");
+        DateTime todayUtc = DateTime.UtcNow;
+        Console.WriteLine($"todayUtc: {todayUtc}");
+        DateTime birthday = DateTime.Parse("9/20/2018");
+        Console.WriteLine($"birthday: {birthday}");
+        DateTime birthdayExact = DateTime.ParseExact("20/9/2018", "d/M/yyyy", CultureInfo.InvariantCulture);
+        Console.WriteLine($"birthdayExact: {birthdayExact.ToString("MMMM dd, yyyy tt zzz")}");
+        Console.ReadLine();
+
+    }
+}
+
