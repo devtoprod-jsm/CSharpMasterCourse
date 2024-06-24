4cad7b6326cb117758016295d5ba3d5356629004
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-24 21:45:30 +0100

    Basic bool datatype app

---
+++ BoolVariableApp/BoolVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BoolVariable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isComplete = true;
+
+        Console.WriteLine(!isComplete);
+        Console.WriteLine(isComplete);
+        Console.ReadLine();
+    }
+}
+
