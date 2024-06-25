cec840dd9b0381b782f447e07daf653f7ab63184
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-25 13:16:39 +0100

    BAsic if app

---
+++ BasicIfApp/BasicIf/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace BasicIf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isComplete = false;
+
+        if (isComplete)
+        {
+            Console.WriteLine("It is true");
+        }
+        else
+        {
+            Console.WriteLine("It is false");
+        }
+
+        Console.Write("Please enter your firstname: ");
+        string? firstName = " ";
+        firstName = Console.ReadLine();
+
+        string? lastName;
+
+        if (firstName.ToLower() == "jai")
+        {
+            Console.WriteLine("Hello Jaisrith");
+            lastName = "Tella";
+        }
+
+        else
+        {
+            Console.WriteLine($"Hello {firstName}");
+            lastName = "Sorry";
+        }
+
+        Console.WriteLine(lastName);
+        Console.ReadLine();
+    }
+}
+
