2dc54322cd4faf0941fa9487174c3098b2b0de68
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-25 17:24:42 +0100

    Advanced if statement app

---
+++ AdvancedIfApp/AdvancedIf/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AdvancedIf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your first name: ");
+        string? firstName = null;
+firstName = Console.ReadLine();
+        Console.Write("Please enter your last name: ");
+        string? lastName = "";
+        lastName = Console.ReadLine();
+
+        if ( (firstName.ToLower() == "jai" || firstName.ToLower() == "jaisrith" ) &&
+            (lastName.ToLower() == "t" || lastName.ToLower() == "Tella") )
+         {
+            Console.WriteLine("Hi son of Mohan");
+        }
+
+        else if( firstName.ToLower() == "mohan" && lastName.ToLower() == "tella")
+        {
+            Console.WriteLine("Hi Jaisrith's father");
+        }
+
+        else
+        {
+            Console.WriteLine("Hi there!");
+        }
+        Console.ReadLine();
+    }
+}
+
