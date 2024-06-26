8638903ed6a8ed0fa4bb7b0a1c54dd07e8e8c10a
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-26 23:26:50 +0100

    Basic dictionariess app

---
+++ DictionariesApp/Dictionaries/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> lookup = new Dictionary<string, string>();
+
+        lookup["fish"] = "Not human that swims";
+        lookup["human"] = "Us";
+        lookup["bird"] = "Not human that flies";
+
+        Console.WriteLine($"The meaning of bird is {lookup["bird"]}");
+        Console.ReadLine();
+    }
+}
+
