02a94773dd39921114911759501430d7aaffeae0
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-27 00:17:45 +0100

    Basic foreach loops app

---
+++ ForEachLoopsApp/ForEachLoops/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ForEachLoops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string data = "Jaisrith,Yuktha,Nikitha,Vivek";
+
+        List<string> firstNames = data.Split(',').ToList();
+
+        foreach (string firstName in firstNames)
+        {
+            Console.WriteLine(firstName);
+        }
+        Console.ReadLine();
+    }
+}
+
