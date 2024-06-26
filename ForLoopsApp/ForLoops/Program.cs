7fd6fcea0ecb930c6c492391537ee75a2e1e89cb
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-27 00:14:33 +0100

    Basic for loops app

---
+++ ForLoopsApp/ForLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ForLoops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> firstNames = new();
+
+        firstNames.Add("Jaisrith");
+        firstNames.Add("Yuktha");
+        firstNames.Add("Nikitha");
+        firstNames.Add("Vivek");
+        for (int i = 0; i < firstNames.Count; i++)
+        {
+            Console.WriteLine(firstNames[i]);
+        }
+       
+        Console.ReadLine();
+    }
+}
+
