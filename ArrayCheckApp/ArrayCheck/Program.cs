4cd5d8f8df40de102ef426377b160565a86cf4e2
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-26 18:56:02 +0100

    Basic Array test app

---
+++ ArrayCheckApp/ArrayCheck/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ArrayCheck;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int choice;
+        bool isValid;
+        string[] firstNames = new string[] { "Jaisrith", "Yuktha", "Nikitha", "Vivek" };
+        do
+        {
+            Console.Write("Choose a number from 1 to 4: ");
+
+            string? choiceText = Console.ReadLine();
+            isValid = int.TryParse(choiceText, out choice);
+
+            if (isValid == false || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Sorry input should be a valid number between 1 and 4 in digits");
+            }
+        } while (isValid == false ||  choice < 1 || choice > 4);
+
+
+        Console.WriteLine($"Hi {firstNames[choice-1]}");
+        Console.ReadLine();
+
+
+    }
+}
+
