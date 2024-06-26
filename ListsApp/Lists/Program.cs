7926e80c85bdf9c3225fc9d3006266565baba56b
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-26 21:32:43 +0100

    Basic lists app

---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> firstNames = new List<string>();
+
+        firstNames.Add("Jaisrith");
+        firstNames.Add("Yuktha");
+        firstNames.Add("Nikitha");
+        firstNames.Add("Vivek");
+
+        Console.WriteLine(firstNames[firstNames.Count -1]);
+
+        string data = "Tella,Guttikonda,Jagarlamudi";
+        List<string> lastNames = data.Split(',').ToList();
+        Console.WriteLine(lastNames[lastNames.Count -1]);
+        Console.ReadLine();
+    }
+}
+
