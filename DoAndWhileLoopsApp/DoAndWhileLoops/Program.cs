f8f99b6d2829500f7dc195f31cc95abb720ac2a3
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-26 14:01:49 +0100

    Basic do and while loop app

---
+++ DoAndWhileLoopsApp/DoAndWhileLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DoAndWhileLoops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isValidAge;
+        int age;
+
+        do
+        {
+            Console.Write("Please enter your age: ");
+            string? ageText = Console.ReadLine();
+            isValidAge = int.TryParse(ageText, out age);
+
+            if (isValidAge == false)
+            {
+                Console.WriteLine("Sorry please enter age in digits");
+            }
+        } while (isValidAge == false);
+
+        Console.WriteLine($"Hi you are {age}");
+
+        int myNumber = 0;
+        do
+        {
+            Console.WriteLine($"It's {myNumber}");
+            myNumber += 3;
+        } while (myNumber < 100);
+        Console.ReadLine();
+    }
+}
+
