72fc923e976a73a08f59ae89b0154ac5ec327c0e
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-26 18:28:01 +0100

    Basic Array app

---
+++ ArraysApp/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Arrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] firstNames = new string[4];
+        firstNames[0] = "Yuktha";
+        firstNames[1] = "Jaisrith";
+        firstNames[2] = "Nikitha";
+        firstNames[3] = "Vivek";
+
+        string[] lastNames = new string[] { "Tella", "Nangineni", "Guttikonda", "Jagarlamudi" };
+        Console.WriteLine(lastNames.Length);
+
+        string stringFirstNames = "Jai,Yuktha,Nikki,Vivek";
+
+        string[] splitNames = stringFirstNames.Split(',');
+
+        Console.WriteLine(splitNames[2]);
+
+        Console.ReadLine();
+    }
+}
+
