0a1531d4587090fc67dbe58110d838dc7d8ec975
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-06-28 17:48:28 +0100

    Basic debugging and exceptions app

---
+++ BasicDebuggingAndExceptionsApp/BasicDebuggingAndExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace BasicDebuggingAndExceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            BadCall();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("There is an error that occured");
+            Console.WriteLine(ex.Message);
+
+        }
+
+        Console.ReadLine();
+    }
+
+    public static void BadCall()
+    {
+        int[] numbers = new int[] { 1, 3, 5 };
+        for (int i = 0; i <= numbers.Length; i++)
+        {
+            try
+            {
+                Console.WriteLine(numbers[i]);
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("There is an error");
+                Console.WriteLine(ex.Message);
+                throw new Exception("There is an error that occured handling the array", ex);
+            }
+        }
+    }
+}
+
