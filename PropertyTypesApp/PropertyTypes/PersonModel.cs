68823e7bc4eb8c834d8aee41c7729da17d4f9f5a
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-07-05 02:34:34 +0100

    Created Property types app

---
+++ PropertyTypesApp/PropertyTypes/PersonModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace PropertyTypes
+{
+	public class PersonModel
+	{
+
+		public string FirstName { private get; set; }
+		public string LastName { get; private set; }
+
+		private string _password;
+
+		public string Password
+		{
+			set
+			{
+				_password = value;
+			}
+		}
+
+		private string _fullname;
+
+		public string FullName
+		{
+			get
+			{
+				return $"{FirstName} {LastName}";
+			}
+		}
+
+		private int _age;
+
+		public int Age
+		{
+			get { return _age; }
+
+			set
+			{
+				if (value >= 0 && value < 125)
+				{
+					_age = value;
+				}
+
+				else
+				{
+					throw new ArgumentOutOfRangeException("value", "Please enter a valid age.");
+				}
+			}
+		}
+
+		private string _ssn; // 123-45-6789
+
+		public string SSN
+		{
+			get
+			{
+				return "***-**-" + _ssn.Substring(_ssn.Length - 4);
+			}
+
+			set
+			{
+				_ssn = value;
+			}
+		}
+
+		public PersonModel()
+		{
+
+		}
+
+		public PersonModel(string lastName)
+		{
+			LastName = lastName;
+		}
+	}
+}
+
---
+++ PropertyTypesApp/PropertyTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PropertyTypes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PersonModel person = new PersonModel("Tella");
+
+        person.FirstName = "Jaisrith";
+        //person.LastName = "Tella";
+        person.Age = 6;
+        person.SSN = "123-45-6789";
+
+        Console.WriteLine(person.SSN);
+        Console.WriteLine(person.FullName);
+        Console.ReadLine();
+    }
+}
+
