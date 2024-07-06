cfe95f7c56c27223f6eeeadd4a788f5af0eef2bd
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-07-06 12:30:49 +0100

    Added guestbook peoject app

---
+++ BetterGuestBookProject/ConsoleUI/Program.cs
@@ -0,0 +1,52 @@
+﻿using GuestBookLibrary.Models;
+
+namespace ConsoleUI;
+
+class Program
+{
+    private static List<GuestModel> guests = new List<GuestModel>();
+
+    static void Main(string[] args)
+    {
+        GetGuestInformation();
+        PrintGuestInformation();
+
+        Console.ReadLine();
+    }
+
+    public static void GetGuestInformation()
+    {
+        string anyMoreGuests = "";
+        //GuestModel guest = new GuestModel();
+
+        do
+        {
+            GuestModel guest = new GuestModel();
+            guest.FirstName = GetInfoFromConsole("What is your firstname? ");
+            guest.LastName = GetInfoFromConsole("What is your lastname? ");
+            guest.MessageToHost = GetInfoFromConsole("Could you please provide a message to the host. ");
+            anyMoreGuests = GetInfoFromConsole("Are there any more guests coming (yes/no)? ");
+
+            guests.Add(guest);
+            Console.Clear();
+
+        } while (anyMoreGuests.ToLower() == "yes");
+    }
+
+    public static void PrintGuestInformation()
+    {
+        foreach (GuestModel guest in guests)
+        {
+            Console.WriteLine(guest.GuestInfo);
+        }
+    }
+
+    public static string GetInfoFromConsole(string message)
+    {
+        string output = "";
+        Console.Write(message);
+        output = Console.ReadLine();
+        return output;
+    }
+}
+
---
+++ BetterGuestBookProject/GuestBookLibrary/Models/GuestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GuestBookLibrary.Models
+{
+	public class GuestModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string MessageToHost { get; set; }
+
+		public string GuestInfo
+		{
+			get
+			{
+				return $"{FirstName} {LastName}: {MessageToHost}";
+			}
+		}
+		public GuestModel()
+		{
+		}
+	}
+}
+
